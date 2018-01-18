e622fdf951bf4f525748068f5a60508600a50ae9
Camboridesbikes <hattendorfcameron@gmail.com>
2018-01-18 12:18:38 -0800

    first commit

---
+++ JS/learn-to-code.js
@@ -0,0 +1,154 @@
+//var name = 'Jack';
+//var age = 23;
+//
+//var message = "Hi, my name is " + name + " and I am "  + age + "years old!";
+
+//var firstName = "John";
+//var lastName = "Doe";
+//var dateOfBirth = "10-09-82";
+//var age = 23;
+//var profileImgUrl = "http://coolpicks.com/johnjacob.jpg";
+//
+//var loginWelcomeMessage = "Welcome, " + firstName + '. Happy ' + age + 'rd birthday!';
+//
+//console.log(loginWelcomeMessage);
+
+//var sum = 10 + 15;
+//var sub = 15 - 10;
+//var mul = 10 * 3;
+//var div = 10 / 3;
+//var mod = 10 % 3;
+//
+//var msg = '10 / 3 = 3 with a remainder of ' + mod;
+//
+//console.log(msg);
+
+//var myAccountBalance = 300;
+//var nikeSBShoes = 799.23;
+//var coupon = 500;
+//
+//if (nikeSBShoes <= myAccountBalance){
+//	myAccountBalance -= nikeSBShoes;
+//	console.log('We Just bought some dope shoes!' + myAccountBalance);
+//} else if (nikeSBShoes - coupon <= myAccountBalance){
+//	myAccountBalance -= nikeSBShoes - coupon;
+//	console.log('we just bought some dope shoes with a coupon!' + myAccountBalance);
+//} else {
+//	console.log('You too broke fo shoes!');
+//}
+
+//if(1 === 1 && 2 === 2 || 'joe' === 'joe'){
+//	console.log('These are both true');
+//}
+//
+//if (true && true){
+//	console.log('these are the same');
+//}
+//
+//if (1 ===3 || 'joe' === 'joe'){
+//	console.log('One of these are true!');
+//}
+
+//var cat1 = 5;
+//var cat2 = 10;
+//var cat3 = 1;
+//var c3disabledHandicap = true;
+//
+//if ((cat1 > cat2 && cat1 > cat3)&& !c3disabledHandicap){
+//	console.log('Cat 1 is the cutest!');
+//} else if ((cat2 > cat1 && cat2 > cat3) && !c3disabledHandicap){
+//	console.log('Cat 2 is the cutest!');
+//} else if ((cat3 > cat1 && cat3 > cat2) || c3disabledHandicap){
+//	console.log('Cat 3 is the cutest!');
+//}
+//var total = 10;
+////define things;condition;occurs at the end of loop
+//for (var x = 0; x < total; x++){
+//	//iterate until told not to
+//	console.log(x);
+//	
+//}
+
+//var students =['John', 'Jacob', 'Jingle', 'Heimer', 'Smith'];
+//
+//for (var x = 0; x < students.length; x++){
+//	console.log(students[x]);
+//}
+
+//var length1 = 15;
+//var width1 = 10;
+//var area1 = length1 * width1;
+//
+//var length2 = 12;
+//var width2 = 14;
+//var area2 = length2 * width2;
+
+
+//
+//console.log(area1);
+//console.log(area2);
+
+//function area(length, width){
+//	return length * width;
+//}
+//
+//var rectanglesAreas = [];
+//rectanglesAreas.push(area(10, 15));
+//rectanglesAreas.push(area(3, 9));
+//rectanglesAreas.push(area(20, 17));
+//
+//console.log(rectanglesAreas);
+//
+//var bankBalance = 500;
+//
+//function makeTransaction(priceOfSale){
+//	if (priceOfSale <= bankBalance){
+//		bankBalance -= priceOfSale;
+//		console.log('Successful Purchase!');
+//		console.log('Your remaining balance is: $' + bankBalance);
+//	} else{
+//		console.log('Isufficient Funds');
+//	}
+//}
+//
+////console.log(bankBalance);
+//makeTransaction(79.00);
+//
+////console.log(bankBalance);
+//makeTransaction(2.32);
+//
+////console.log(bankBalance);
+//makeTransaction(10.45);
+//
+////console.log(bankBalance);
+//makeTransaction(450.00);
+//
+//var transaction = function(priceOfSale){
+//	if (priceOfSale <= bankBalance){
+//		bankBalance -= priceOfSale;
+//		console.log('Successful Purchase!');
+//		console.log('Your remaining balance is: $' + bankBalance);
+//	} else{
+//		console.log('Isufficient Funds');
+//	}
+//};
+//
+//var printCustomerName = function(first,last){
+//	console.log('First Name: ' + first+ 
+//			   'Last Name: ' + last);
+//};
+//
+//var applyForCreditCard = function(creditScore, soul){
+//	//Do some stuff
+//};
+//
+//var bankOperations = [];
+//bankOperations.push(transaction);
+//bankOperations.push(printCustomerName);
+//bankOperations.push(applyForCreditCard);
+
+
+
+
+
+
